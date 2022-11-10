68275417bf6468f90a018c73fba93d44d89844ca
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2022-11-09 20:40:23 -0500

    album list and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var searchInput = document.querySelector('#search-input')
 var searchBtn = document.querySelector('#search-btn')
 var coverArtArr = document.querySelectorAll('.cover-art')
+var albumNameArr = document.querySelectorAll('.album-name')
+var artistNameEle = document.querySelector('#artist-name')
+var searchHistoryEle = document.querySelector('#search-history')
 searchBtn.addEventListener('click', function() {
     searchArtist(searchInput.value)
 })
@@ -10,17 +13,22 @@ function searchArtist(artistName) {
     .then(function (response) { return response.json()} )
     .then(function(searchResult) {
         var albumsArr = searchResult["release-groups"]
+        if (albumsArr === []) {
+            alert("No artist found!")
+        }
         console.log(albumsArr)
+        artistNameEle.innerText = albumsArr[0]["artist-credit"][0].name
         for (let i = 0; i < albumsArr.length; i++) {
-            const albumObj = albumsArr[i];
+            const albumObj = albumsArr[i].releases[0];
             var albumID = albumObj.id
             displayCover(albumID, i)
+            displayAlbumName(albumObj.title, i)
         }
     })
 }
 
 function displayCover(MBID, index) {
-    fetch(`https://coverartarchive.org/release/${MBID}`)
+    fetch(`http://coverartarchive.org/release/${MBID}`)
     .then(function (response) { return response.json()} )
     .then(function(searchResult) {
         console.log(searchResult)
@@ -29,16 +37,34 @@ function displayCover(MBID, index) {
     })
 }
 
+function displayAlbumName(name, index) {
+    albumNameArr[index].innerHTML = name
+}
 
+function getSearchHistory() {
+    var result = localStorage.getItem("searchHistory")
+    if (result) {
+        result = JSON.parse(result)
+    }
+    else {
+        result = []
+    }
+    return result
+}
 
+function addSearchHistory(item) {
+    var searchHistory = getSearchHistory()
+    searchHistory.unshift(item)
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory))
+}
 
-// function renderSearchHistory(cityName) {
-//     searchHistoryEl.empty();
-//     let searchHistoryArr = JSON.parse(localStorage.getItem("searchHistory"));
-//     for (let i = 0; i < searchHistoryArr.length; i++) {
-//         // newListItem in loop because otherwise the text of the li element changes, rather than making a new element for each array index
-//         let newListItem = $("<li>").attr("class", "historyEntry");
-//         newListItem.text(searchHistoryArr[i]);
-//         searchHistoryEl.prepend(newListItem);
-//     }
-// }
+function renderSearchHistory() {
+    searchHistoryEl.empty()
+    let searchHistoryArr = getSearchHistory()
+    for (let i = 0; i < searchHistoryArr.length; i++) {
+        // newListItem in loop because otherwise the text of the li element changes, rather than making a new element for each array index
+        let newListItem = $("<li class='historyEntry'>")
+        newListItem.text(searchHistoryArr[i])
+        searchHistoryEl.prepend(newListItem)
+    }
+}
