0369ffa38c35901e0c0a8adb490bc31a08afc54d
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2022-11-10 22:39:25 -0500

    kinda works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,21 @@
 var searchInput = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
+var searchHistoryEle = document.querySelector("#search-history");
 var coverArtArr = document.querySelectorAll(".cover-art");
 var albumNameArr = document.querySelectorAll(".album-name");
 var artistNameEle = document.querySelector("#artist-name");
-var searchHistoryEle = document.querySelector("#search-history");
+var recArtistNameArr = document.querySelectorAll(".rec-artist-name")
+var recArtistImgArr = document.querySelectorAll(".rec-artist-img")
 searchBtn.addEventListener("click", function () {
   searchArtist(searchInput.value);
 });
 
 function searchArtist(artistName, addToHistory = true) {
-  fetch(
-    `http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`
-  )
-    .then(function (response) {
-      return response.json();
-    })
+  fetch(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
+    .then(function (response) {return response.json();})
     .then(function (searchResult) {
       var albumsArr = searchResult["release-groups"];
       var artistName = albumsArr[0]["artist-credit"][0].name;
-
-      console.log(addToHistory);
 
       if (addToHistory) {
         addSearchHistory(artistName);
@@ -29,26 +25,28 @@ function searchArtist(artistName, addToHistory = true) {
       if (albumsArr === []) {
         alert("No artist found!");
       }
+
       artistNameEle.innerText = artistName;
       for (let i = 0; i < albumsArr.length; i++) {
         const albumObj = albumsArr[i].releases[0];
         var albumID = albumObj.id;
-        displayCover(albumID, i);
+        displayCover(albumID, coverArtArr[i]);
         displayAlbumName(albumObj.title, i);
       }
       console.log("hi");
     });
 }
 
-function displayCover(MBID, index) {
+function displayCover(MBID, imgEle) {
   fetch(`https://coverartarchive.org/release/${MBID}`)
     .then(function (response) {
+      if (!response.ok) { return }
       return response.json();
     })
     .then(function (searchResult) {
-      var imgURL = searchResult.images[0].image;
-      coverArtArr[index].setAttribute("src", imgURL);
-    });
+      var imgURL = searchResult.images[0].image || 'https://placeholder.pics/svg/300';
+      imgEle.setAttribute("src", imgURL);
+    }).catch( (err) => {} )
 }
 
 function displayAlbumName(name, index) {
@@ -56,129 +54,38 @@ function displayAlbumName(name, index) {
 }
 
 function getReccomendations(artistName) {
-    const request = {
-        headers: {
-            "Authorization": "Bearer 444444-NoStopMu-WI4FVWTE",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "http://localhost:5500"
-        }
-    }
-//   fetch(
-//     `https://tastedive.com/api/similar?q=band:${artistName}&type=music&info=1`, request
-//   )
-//     .then(function (response) {
-//         console.log(response.headers)
-//       return response.json();
-//     })
-//     .then(function (searchResults) {
-//       console.log(searchResults);
-//     });
-    
-    const promise = new Promise((resolve, reject) => {
-        return setTimeout(() => {
-            if (request && request.headers["Authorization"] === "Bearer 444444-NoStopMu-WI4FVWTE" && typeof artistName === "string" && true) {
-                return resolve({"Similar": {
-                    "Info": [
-                      {
-                        "Name": "Red Hot Chili Peppers",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Pulp Fiction",
-                        "Type": "movie"
-                      }
-                    ],
-                    "Results": [
-                      {
-                        "Name": "Nirvana",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Foo Fighters",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "System Of A Down",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "The White Stripes",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Green Day",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Oasis",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Incubus",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Linkin Park",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Muse",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "The Offspring",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "The Beatles",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Rage Against The Machine",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Blink-182",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Pearl Jam",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "The Killers",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Radiohead",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Led Zeppelin",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Coldplay",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Gorillaz",
-                        "Type": "music"
-                      },
-                      {
-                        "Name": "Metallica",
-                        "Type": "music"
-                      }
-                    ]
-                  }})
-                }
-                return reject()
-        }, 3000);
-    })
-    return promise.then( (data) => console.log(data))
+    var urlPrefix = (!document.location.href.includes('https'))?'https://corsanywhere.herokuapp.com/':''
+  fetch(
+    `${urlPrefix}https://tastedive.com/api/similar?q=${artistName}&type=music&info=1&k=444444-NoStopMu-WI4FVWTE`
+  )
+    .then(function (response) {return response.json();} )
+    .then(function (searchResults) {
+      searchResults = searchResults.Similar.Results
+      displayRecArtists(searchResults)
+    });
 }
 
+async function displayRecArtists(resultsList)  {
+    for (let i = 0; i < recArtistNameArr.length; i++) {
+        var name = resultsList[i].Name
+        recArtistNameArr[i].innerText = name
 
+        var artistImgEle = recArtistImgArr[i]
+        var albums = await getAlbums(name)
+        console.log(albums)
+        displayCover(albums.id, artistImgEle)
+    }
+}
+
+async function getAlbums(artistName) {
+    albumsArr = await fetch(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
+    .then(function (response) {return response.json();})
+    .then( (results) => {
+        albumsArr = results["release-groups"]
+        return albumsArr
+    })
+    return albumsArr
+}
 
 function getSearchHistory() {
   var result = localStorage.getItem("searchHistory");
