bdb7a3d03ec0715e6613565ea68a2846e1d7941a
Jared Schips <110642249+JaredSchips@users.noreply.github.com>
2022-11-13 17:34:44 -0500

    finalizing features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,132 @@
+var urlPrefix = (!document.location.href.includes('https'))?'https://corsanywhere.herokuapp.com/':''
+
 var searchInput = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
 var searchHistoryEle = document.querySelector("#search-history");
+var imgBtnArr = document.querySelectorAll(".img-button")
 var coverArtArr = document.querySelectorAll(".cover-art");
 var albumNameArr = document.querySelectorAll(".album-name");
 var artistNameEle = document.querySelector("#artist-name");
-var recArtistNameArr = document.querySelectorAll(".rec-artist-name")
 var recArtistImgArr = document.querySelectorAll(".rec-artist-img")
+var recArtistNameArr = document.querySelectorAll(".rec-artist-name")
 searchBtn.addEventListener("click", function () {
   searchArtist(searchInput.value);
 });
+searchInput.addEventListener("keypress", (event) => {
+  if (event.key === 'Enter') {
+    searchArtist(searchInput.value)
+  }
+})
 
-function searchArtist(artistName, addToHistory = true) {
-  fetch(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
-    .then(function (response) {return response.json();})
-    .then(function (searchResult) {
-      var albumsArr = searchResult["release-groups"];
-      var artistName = albumsArr[0]["artist-credit"][0].name;
-
-      if (addToHistory) {
-        addSearchHistory(artistName);
-        renderSearchHistory();
-      }
-
-      if (albumsArr === []) {
-        alert("No artist found!");
-      }
-
-      artistNameEle.innerText = artistName;
-      for (let i = 0; i < albumsArr.length; i++) {
-        const albumObj = albumsArr[i].releases[0];
-        var albumID = albumObj.id;
-        displayCover(albumID, coverArtArr[i]);
-        displayAlbumName(albumObj.title, i);
-      }
-      console.log("hi");
-    });
+async function get(url) {
+  return await fetch(url)
+  .then( (response) => {
+    if (!response.ok) { return }
+    return response.json()
+  })
+  .then( (data) => {
+    return data
+  })
 }
 
-function displayCover(MBID, imgEle) {
-  fetch(`https://coverartarchive.org/release/${MBID}`)
-    .then(function (response) {
-      if (!response.ok) { return }
-      return response.json();
-    })
-    .then(function (searchResult) {
-      var imgURL = searchResult.images[0].image || 'https://placeholder.pics/svg/300';
-      imgEle.setAttribute("src", imgURL);
-    }).catch( (err) => {} )
+async function searchArtist(artistName, addToHistory = true) {
+  searchInput.value = ''
+  clearButtons()
+  artistNameEle.innerText = ''
+  clearAll(coverArtArr)
+  clearAll(albumNameArr)
+  clearAll(recArtistImgArr)
+  clearAll(recArtistNameArr)
+
+  document.querySelector(':root').style.setProperty('--results-display', 'flex')
+
+  var searchResult = await get(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
+
+  if (searchResult.count === 0) {
+    alert("No artist found!");
+  }
+  console.log(searchResult)
+
+  var releaseGroupArr = searchResult["release-groups"];
+  var artistName = releaseGroupArr[0]["artist-credit"][0].name;
+
+  artistNameEle.innerText = artistName;
+
+  if (getSearchHistory()[0] === artistName) {
+    addToHistory = false
+  }
+  if (addToHistory) {
+    addSearchHistory(artistName);
+    renderSearchHistory();
+  }
+
+  for (let i = 0; i < coverArtArr.length; i++) {
+    var releaseGroup = releaseGroupArr[i];
+    if (releaseGroup) {
+      displayCover(releaseGroup, coverArtArr[i]);
+      displayAlbumName(releaseGroup.title, i);
+    }
+  }
+  displayRecArtists(artistName);
+}
+
+function clearButtons() {
+  for (let i = 0; i < imgBtnArr.length; i++) {
+    imgBtnArr[i].style.display = 'none'
+  }
+}
+
+function clearAll(eleArr) {
+  for (let i = 0; i < eleArr.length; i++) {
+    ele = eleArr[i]
+    if (ele.tagName === 'IMG') {
+      ele.setAttribute('src', '');
+    }
+    else if (ele.tagName === 'P') {
+      ele.innerText = ''
+    }
+  }
+}
+
+async function displayCover(releaseGroup, imgEle) {
+  imgEle.parentElement.style.display = 'inline-block'
+
+  var imgURL = '';
+  for (let i = 0; i < releaseGroup.releases.length; i++) {
+    var release = releaseGroup.releases[i];
+    var searchResult = await get(`https://coverartarchive.org/release/${release.id}`)
+    if (searchResult) { 
+      imgURL = searchResult.images[0].image;
+      break;
+    }
+  }
+  imgEle.setAttribute('src', imgURL);
 }
 
 function displayAlbumName(name, index) {
   albumNameArr[index].innerHTML = name;
 }
 
-function getReccomendations(artistName) {
-    var urlPrefix = (!document.location.href.includes('https'))?'https://corsanywhere.herokuapp.com/':''
-  fetch(
-    `${urlPrefix}https://tastedive.com/api/similar?q=${artistName}&type=music&info=1&k=444444-NoStopMu-WI4FVWTE`
-  )
-    .then(function (response) {return response.json();} )
-    .then(function (searchResults) {
-      searchResults = searchResults.Similar.Results
-      displayRecArtists(searchResults)
-    });
-}
-
-async function displayRecArtists(resultsList)  {
+async function displayRecArtists(artistName)  {
+  var searchResults = await get(`${urlPrefix}https://tastedive.com/api/similar?q=${artistName}&type=music&k=444444-NoStopMu-WI4FVWTE`)
+  var resultsList = searchResults.Similar.Results
     for (let i = 0; i < recArtistNameArr.length; i++) {
         var name = resultsList[i].Name
         recArtistNameArr[i].innerText = name
 
         var artistImgEle = recArtistImgArr[i]
-        var albums = await getAlbums(name)
-        console.log(albums)
-        displayCover(albums.id, artistImgEle)
+        var releaseGroups = await getAlbums(name)
+
+        artistImgEle.parentElement.setAttribute('onclick', `searchArtist('${name}')`)
+        console.log(artistImgEle)
+
+        displayCover(releaseGroups[0], artistImgEle)
     }
 }
 
 async function getAlbums(artistName) {
-    albumsArr = await fetch(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
-    .then(function (response) {return response.json();})
-    .then( (results) => {
-        albumsArr = results["release-groups"]
-        return albumsArr
-    })
-    return albumsArr
+    var results = await get(`http://musicbrainz.org/ws/2/release-group?query=artist:"${artistName}"%20AND%20NOT%20secondarytype:*%20AND%20primarytype:album&fmt=json`)
+    return results["release-groups"]
 }
 
 function getSearchHistory() {
@@ -98,7 +140,6 @@ function getSearchHistory() {
 }
 
 function addSearchHistory(item) {
-  console.log(`adding ${item}`);
   var searchHistory = getSearchHistory();
   searchHistory.unshift(item);
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
